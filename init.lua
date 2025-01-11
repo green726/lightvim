b47a73b6c4900e8fd162e62430b4875832ca1545
Milan Lustig <mguinangreen726@gmail.com>
2025-01-11 13:34:17 -0500

    changed lazy

--- init.lua
+++ init.lua
@@ -1,20 +1,6 @@
 vim.cmd([[let g:copilot_assume_mapped = v:true]])
 vim.cmd("let g:copilot_no_tab_map = v:true")
 
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("plugins/lazy")
 
 --Stop the stupid pasting over thingy
